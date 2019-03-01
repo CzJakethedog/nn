447e65f637c7780b03c0aab65252d4da1258c2b7
CzTay3103 <34021885+CzJakethedog@users.noreply.github.com>
2019-03-01 10:23:57 +0800

    Update __init__.py

--- conv/__init__.py
+++ conv/__init__.py
@@ -1,7 +1,2 @@
-from .shallownet import ShallowNet
-from .lenet import LeNet
-from .minivggnet import MiniVGGNet
 from .alexnet import AlexNet
-from .minigooglenet import MiniGoogLeNet
-from .deepergooglenet import DeeperGoogLeNet
-from .resnet import ResNet
+
